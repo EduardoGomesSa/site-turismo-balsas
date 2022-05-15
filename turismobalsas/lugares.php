806095339add93f40e55c8f5485d653099c82e28
EDUARDO GOMES DE SA <99502227+EduardoGomesSa@users.noreply.github.com>
2022-05-14 21:57:05 -0300

    conclusão da mesclagem de CSS

--- turismobalsas/lugares.php
+++ turismobalsas/lugares.php
@@ -15,47 +15,47 @@
         <?php require_once "_issets/cabecalho.php" ?>
         <article class="paginas">
             <div class="esquerda1">
-                <div class="padraoS lugar1">
+                <div class="padrao lugar1">
                     <h1>lugar 1</h1>
                 </div>
-                <div class="lugar2">
+                <div class="padrao lugar2">
                     <h1>lugar 2</h1>
                 </div>
             </div>
-            <div class="lugar3">
+            <div class="padrao lugar3">
                 <h1>lugar 3</h1>
             </div>
             <div class="esquerda2">
-                <div class="lugar4">
+                <div class="padrao lugar4">
                     <h1>lugar 4</h1>
                 </div>
-                <div class="lugar5">
+                <div class="padrao lugar5">
                     <h1>lugar 5</h1>
                 </div>
             </div>
-            <div class="lugar6">
+            <div class="padrao lugar6">
                 <h1>lugar 6</h1>
             </div>
             <div class="esquerda3">
-                <div class="lugar7">
+                <div class="padrao lugar7">
                     <h1>lugar 7</h1>
                 </div>
-                <div class="lugar8">
+                <div class="padrao lugar8">
                     <h1>lugar 8</h1>
                 </div>
             </div>
-            <div class="lugar9">
+            <div class="padrao lugar9">
                 <h1>lugar 9</h1>
             </div>
             <div class="esquerda4">
-                <div class="lugar10">
+                <div class="padrao lugar10">
                     <h1>lugar 10</h1>
                 </div>
-                <div class="lugar11">
+                <div class="padrao lugar11">
                     <h1>lugar 11</h1>
                 </div>
             </div>
-            <div class="lugar12">
+            <div class="padrao lugar12">
                 <h1>lugar 12</h1>
             </div>
         </article>
